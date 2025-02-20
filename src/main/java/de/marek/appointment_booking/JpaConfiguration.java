8dbacea6a0d8f72618609e7e005134d706d0efb2
tvedeane <1138746+tvedeane@users.noreply.github.com>
2025-02-20 12:25:36 +0100

    Move JPA config to separate class

---
+++ src/main/java/de/marek/appointment_booking/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package de.marek.appointment_booking;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class JpaConfiguration {
+}
